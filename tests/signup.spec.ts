d4fb9f3e89a2d3e1fa4bf3ac26fd38a4a403045f
B0rtleinacan <Cloverlyn123@gmail.com>
2026-07-12 18:07:41 -0500

    commented out unfinished tests

--- tests/signup.spec.ts
+++ tests/signup.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect, type Page } from '@playwright/test';
 import { MainPage } from '../pages/MainPage';
 
+/*
 test('Sign up can be clicked and takes the user to the sign up page', async ({ page }: { page: Page }) => {
     const mainPage = new MainPage(page);
 
@@ -8,4 +9,5 @@ test('Sign up can be clicked and takes the user to the sign up page', async ({ p
     await mainPage.signUpLink.click();
 
     expect(mainPage.page.getByText('Create your account'));    
-})
+});
+*/
